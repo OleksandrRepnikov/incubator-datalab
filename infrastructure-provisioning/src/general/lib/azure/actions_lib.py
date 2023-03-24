971615c271dd651103d75e714c292fdf2c8cc104
Oleksandr <korsicanec@hotmail.com>
2023-03-24 13:46:36 +0200

    Delete first condition with if

--- infrastructure-provisioning/src/general/lib/azure/actions_lib.py
+++ infrastructure-provisioning/src/general/lib/azure/actions_lib.py
@@ -1078,8 +1078,6 @@ class AzureActions:
                 "ip_configurations": ip_params
             }
 
-#            if not security_group and ("azure_disable_project_SG_creation" not in os.environ):
-#                raise Exception
             if not security_group and ("azure_disable_project_SG_creation" in os.environ):
                 pass
             elif security_group and ("azure_disable_project_SG_creation" in os.environ):
